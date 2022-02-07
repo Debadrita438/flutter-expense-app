3a5e2b56152adac9a6d05ca33d1d5da10f900d80
Debadrita438 <bristibandyopadhyay@gmail.com>
2022-02-07 10:37:21 +0530

    implemented chart bar

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -14,6 +14,39 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        FittedBox(
+          child: Text('₹${spendingAmount.toStringAsFixed(0)}'),
+        ),
+        SizedBox(height: 4),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 4),
+        Text(label)
+      ],
+    );
   }
 }
